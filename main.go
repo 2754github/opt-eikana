b07b929c68d87360d62f190b2194aa948af6a58a
2754github <fkesys@gmail.com>
2026-07-02 16:43:35 +0900

    use TISSelectInputSource

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 /*
 #cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Carbon -framework CoreGraphics -framework CoreFoundation
+#cgo LDFLAGS: -framework Carbon
 
 #include <Carbon/Carbon.h>
-#include <CoreGraphics/CoreGraphics.h>
 
-#define MY_kVK_JIS_Eisu    0x66
-#define MY_kVK_JIS_Kana    0x68
 #define MY_kVK_Option      0x3A
 #define MY_kVK_RightOption 0x3D
 
@@ -17,13 +14,38 @@ static int otherKeyPressed = 0;
 static int leftOptionDown  = 0;
 static int rightOptionDown = 0;
 
-static void sendKey(CGKeyCode keyCode) {
-    CGEventRef down = CGEventCreateKeyboardEvent(NULL, keyCode, true);
-    CGEventRef up   = CGEventCreateKeyboardEvent(NULL, keyCode, false);
-    CGEventPost(kCGSessionEventTap, down);
-    CGEventPost(kCGSessionEventTap, up);
-    CFRelease(down);
-    CFRelease(up);
+static void selectInputSource(int asciiCapable) {
+    CFDictionaryRef filter = CFDictionaryCreate(
+        kCFAllocatorDefault,
+        (const void *[]){
+            kTISPropertyInputSourceCategory,
+            kTISPropertyInputSourceIsEnabled,
+            kTISPropertyInputSourceIsSelectCapable,
+        },
+        (const void *[]){
+            kTISCategoryKeyboardInputSource,
+            kCFBooleanTrue,
+            kCFBooleanTrue,
+        },
+        3,
+        &kCFTypeDictionaryKeyCallBacks,
+        &kCFTypeDictionaryValueCallBacks
+    );
+    CFArrayRef sources = TISCreateInputSourceList(filter, false);
+    CFRelease(filter);
+    if (!sources) return;
+
+    CFIndex count = CFArrayGetCount(sources);
+    for (CFIndex i = 0; i < count; i++) {
+        TISInputSourceRef src = (TISInputSourceRef)CFArrayGetValueAtIndex(sources, i);
+        CFBooleanRef isASCII = TISGetInputSourceProperty(src, kTISPropertyInputSourceIsASCIICapable);
+        int srcIsASCII = (isASCII == kCFBooleanTrue);
+        if (srcIsASCII == asciiCapable) {
+            TISSelectInputSource(src);
+            break;
+        }
+    }
+    CFRelease(sources);
 }
 
 static CGEventRef eventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
@@ -43,14 +65,14 @@ static CGEventRef eventCallback(CGEventTapProxy proxy, CGEventType type, CGEvent
 
         if (keyCode == MY_kVK_Option && !(flags & kCGEventFlagMaskAlternate)) {
             if (leftOptionDown && !otherKeyPressed) {
-                sendKey(MY_kVK_JIS_Eisu);
+                selectInputSource(1);
             }
             leftOptionDown = 0;
             if (!rightOptionDown) otherKeyPressed = 0;
             return NULL;
         } else if (keyCode == MY_kVK_RightOption && !(flags & kCGEventFlagMaskAlternate)) {
             if (rightOptionDown && !otherKeyPressed) {
-                sendKey(MY_kVK_JIS_Kana);
+                selectInputSource(0);
             }
             rightOptionDown = 0;
             if (!leftOptionDown) otherKeyPressed = 0;
